5e7898070b9dd06d365ffd184dcf67445dbf09bf
jily <jily1218@163.com>
2025-03-03 18:02:40 +0800

    docs(thread): 将线程创建方式的笔记移至资源文件夹并优化内容
    - 将线程创建方式的笔记从 README.md 移至 src/main/resources/笔记md/1.获得线程的方式.md
    - 优化了笔记内容，增加了更多示例代码和详细解释
    - 更新了 ThreadDemo.java 中的注释以与新笔记保持一致

--- src/main/java/com/jily/demo/thread/MyThreadPoolDemo.java
+++
@@ -1,5 +0,0 @@
-package com.jily.demo.thread;
-
-//第四种获得Java多线程的方式-线程池
-public class MyThreadPoolDemo {
-}
--- src/main/java/com/jily/demo/thread/ThreadDemo.java
+++ src/main/java/com/jily/demo/thread/ThreadDemo.java
@@ -1,7 +1,6 @@
 package com.jily.demo.thread;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 //线程创建的四种方式
 public class ThreadDemo {
@@ -17,16 +16,10 @@ public class ThreadDemo {
         try {
 
             //使用Callable接口创建线程时，使用FutureTask作为中间桥梁
-//            基本概念
-            //1.Thread类只能接收Runnable接口的实现类作为参数
-            //2.Callable接口是一个可以有返回值的任务接口
-            //3.FutureTask是一个桥接器，它同时实现了Runnable和Future接口
-
-//            为什么需要FutureTask:
-//            Callable接口不能直接被Thread使用，因为Thread的构造器只接受Runnable
-//            FutureTask实现了Runnable接口，所以它可以被传递给Thread
-//            FutureTask可以包装Callable，使其能够被Thread使用
-//           FutureTask还提供了获取任务执行结果的能力
+//          1 Callable接口不能直接被Thread使用，因为Thread的构造器只接受Runnable
+//          2  FutureTask实现了Runnable接口，所以它可以被传递给Thread
+//          3 FutureTask可以包装Callable，使其能够被Thread使用
+//          4 FutureTask还提供了获取任务执行结果的能力
 
             MyCallable myCallable = new MyCallable();
             FutureTask<String> futureTask = new FutureTask<>(myCallable);
@@ -34,13 +27,19 @@ public class ThreadDemo {
             thread2.start();
 //            可以通过futureTask.get()获取线程执行的结果
             String s = futureTask.get();//获取任务执行结果
-            System.out.println(s);
-
+            System.out.println("线程返回值：" + s);
 
 
             //使用线程池创建线程
+            //这段代码创建了一个线程池，核心线程数为3，最大线程数为5，空闲线程存活时间为1秒，任务队列容量为3，使用默认线程工厂和丢弃策略。
+            ExecutorService threadPool = new ThreadPoolExecutor(3,5,1L, TimeUnit.SECONDS
+            , new LinkedBlockingQueue<Runnable>(3),
+                    Executors.defaultThreadFactory(),
+                    new ThreadPoolExecutor.DiscardPolicy());
+            for (int i = 0; i < 5; i++) {
+                threadPool.execute(new MyRunnable2());
+            }
 
-            ThreadPoolDemo.testThreadPool();
         } catch (Exception e) {
            System.out.println(e.getMessage());
         }
@@ -52,7 +51,7 @@ public class ThreadDemo {
 class MyThread extends Thread {
     @Override
     public void run() {
-        System.out.println("MyThread run");
+        System.out.println("方法1 线程运行中：" + Thread.currentThread().getName());
     }
 }
 
@@ -60,7 +59,7 @@ class MyThread extends Thread {
 class MyRunnable implements Runnable {
     @Override
     public void run() {
-        System.out.println("MyRunnable run");
+        System.out.println("方法2 MyRunnable线程运行中：" + Thread.currentThread().getName());
     }
 }
 
@@ -68,8 +67,16 @@ class MyRunnable implements Runnable {
 class MyCallable implements Callable<String> {
     @Override
     public String call() throws Exception {
-        System.out.println("MyCallable run");
+        System.out.println("方法3 MyCallable run"+ Thread.currentThread().getName());
         return "MyCallable return string";
     }
 }
 
+
+//为线程池写的Runnable2
+class MyRunnable2 implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("方法4 MyRunnable2线程运行中：" + Thread.currentThread().getName());
+    }
+}
